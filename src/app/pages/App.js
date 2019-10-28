ca54d54d9a3d72ba3b167a63620fc3418520a6d9
LeuAlmeida <leo@webid.net.br>
2019-10-28 15:32:04 -0300

    Individual pages creating

--- src/app/pages/App.js
+++ src/app/pages/App.js
@@ -6,14 +6,9 @@ import {
   Link
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+import Home from './Home/index';
+import About from './About/index';
+import Dashboard from './Dashboard/index';
 
 export default function BasicExample() {
   return (
@@ -45,32 +40,5 @@ export default function BasicExample() {
         </Switch>
       </div>
     </Router>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
   );
 }
